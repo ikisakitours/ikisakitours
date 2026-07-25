08202ce54d0fe86c8d0439380616b63dc5fa4b5f
Pramod-Siriwardhana <pramodsiriwardhana@outlook.com>
2026-07-26 05:17:26 +0530

    update: update chat widget configurations and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Playfair_Display, Plus_Jakarta_Sans } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
 import Preloader from "@/components/ui/Preloader";
+import Script from "next/script";
 
 const plusJakarta = Plus_Jakarta_Sans({
   variable: "--font-plus-jakarta",
@@ -66,9 +67,22 @@ export default function RootLayout({
       className={`${plusJakarta.variable} ${playfair.variable} h-full scroll-smooth antialiased`}
     >
       <body className="min-h-full bg-lanka-dark text-slate-200">
-        {/* <Preloader /> */}
+        <Preloader />
         {children}
         <Toaster position="top-center" richColors />
+        <Script id="tawk-to" strategy="lazyOnload">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6a61fe0339cd571d47f5d6a8/1ju7cgg9t';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
