529f111b81e40acb52eba162e20ce94e99d683ed
Pramod-Siriwardhana <pramodsiriwardhana@outlook.com>
2026-07-27 21:32:17 +0530

    update: update chat bot.

--- src/components/ui/TawkToChat.tsx
+++ src/components/ui/TawkToChat.tsx
@@ -68,13 +68,7 @@ export default function TawkToChat() {
 
   return (
     <Script id="tawk-to" strategy="lazyOnload">
-      {`
-       var Tawk_API = Tawk_API || {};
-        Tawk_LoadStart = new Date();
-        
-        Tawk_API.visitor = {
-        };
-        (function(){
+      {`(function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true;
         s1.src='https://embed.tawk.to/6a61fe0339cd571d47f5d6a8/1ju7cgg9t';
